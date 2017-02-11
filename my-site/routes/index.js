5f342d3aaa71b7eb52c3e3331129aff7c67c3968
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-02-11 15:46:46 -0200

    Project started: Register products added

---
+++ my-site/routes/index.js
@@ -0,0 +1,27 @@
+var keystone = require('keystone');
+var middleware = require('./middleware');
+var importRoutes = keystone.importer(__dirname);
+
+// Common Middleware
+keystone.pre('routes', middleware.initLocals);
+keystone.pre('render', middleware.flashMessages);
+
+// Import Route Controllers
+var routes = {
+	views: importRoutes('./views'),
+};
+
+// Setup Route Bindings
+exports = module.exports = function (app) {
+	// Views
+	app.get('/', routes.views.index);
+	app.get('/blog/:category?', routes.views.blog);
+	app.get('/blog/post/:post', routes.views.post);
+	app.get('/gallery', routes.views.gallery);
+	app.all('/contact', routes.views.contact);
+
+	app.get('/produtos', routes.views.produtos);
+	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+	// app.get('/protected', middleware.requireUser, routes.views.protected);
+
+};
